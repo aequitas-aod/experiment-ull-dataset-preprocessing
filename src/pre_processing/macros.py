71e6ec26bb03318e68844f0e58293e50ef2ce4fb
Joseph Giovanelli <j.giovanelli@unibo.it>
2024-04-02 15:30:56 +0200

    Revert "fix: relative path macro"
    This reverts commit a3679f4319475998088cffc8d1b944b2d63cade1.

--- src/pre_processing/macros.py
+++ src/pre_processing/macros.py
@@ -1,7 +1,4 @@
 import os
-from data import PATH as DATA_PATH_COPY
-from data.split import PATH as DATA_SPLIT_PATH_COPY
-from data.pre_processed import PATH as DATA_PREPROC_PATH_COPY
 
 column_groups = {
     "identifiers": [
@@ -581,7 +578,7 @@ column_groups = {
 }
 
 HOME_PATH = os.path.join("/", "home")
-DATA_PATH = DATA_PATH_COPY
-DATA_SPLIT_PATH = DATA_SPLIT_PATH_COPY
-DATA_PREPROC_PATH = DATA_PREPROC_PATH_COPY
+DATA_PATH = os.path.join(HOME_PATH, "data")
+DATA_SPLIT_PATH = os.path.join(DATA_PATH, "split")
+DATA_PREPROC_PATH = os.path.join(DATA_PATH, "pre_processed")
 ORIGINAL_DATASET_NAME = "original.csv"
