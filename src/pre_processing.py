bd2bbd06778c63d2495e6926f244aceb66e42971
Matteo Magnini <matteo.magnini@unibo.it>
2024-05-01 11:05:45 +0200

    feat: added code to find MCAR columns

--- src/pre_processing.py
+++ src/pre_processing.py
@@ -1,8 +1,7 @@
 import os
 
 import pandas as pd
-
-from src.pre_processing.macros import DATA_PREPROC_PATH
+from src.pre_processing.macros import DATA_PREPROC_PATH, DATA_SPLIT_PATH
 from src.pre_processing.principal_questionnaire import (
     preprocess_principal_questionnaire,
 )
@@ -31,6 +30,21 @@ def main():
     print("\tTeacher Questionnaire")
     merged_df = pd.merge(merged_df, teacher_df, left_index=True, right_index=True)
     merged_df.to_csv(os.path.join(DATA_PREPROC_PATH, "merged.csv"))
+    print("\tAdd indices")
+    indices_df = pd.read_csv(os.path.join(DATA_SPLIT_PATH, "identifiers.csv"), low_memory=False)[["id_student","id_year"]]
+    indices_df = indices_df.set_index("id_student")
+
+    # If a column is nan for at least one year (id_year), then it is a MCAR type of missing data
+    null_df = merged_df.isnull()
+    null_df_id_year = pd.merge(null_df, indices_df, left_index=True, right_index=True)
+    mcar = null_df_id_year.groupby("id_year").all().sum(axis=0)
+    mcar = mcar[mcar > 0].index
+    print("\tMCAR columns")
+    print(mcar)
+
+    merged_df = merged_df.drop(columns=mcar)
+    merged_df.to_csv(os.path.join(DATA_PREPROC_PATH, "merged.csv"))
+
     print("Done!")
 
 
