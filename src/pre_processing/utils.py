b1346b974412d5338c84d62d1260f3bee3be0055
Joseph Giovanelli <j.giovanelli@unibo.it>
2024-05-08 18:38:43 +0200

    Update utils.py

--- src/pre_processing/utils.py
+++ src/pre_processing/utils.py
@@ -1,24 +1,36 @@
 import pandas as pd
-import numpy as np  
+import numpy as np
+
 
 def get_feature_list_indexes(df: pd.DataFrame, feature_lists: dict):
     feature_list_idxs = {}
     for k in feature_lists.keys():
-        feature_list_idxs[k] = [list(df.columns).index(x) for x in feature_lists[k] if x in df.columns]
+        feature_list_idxs[k] = [
+            list(df.columns).index(x) for x in feature_lists[k] if x in df.columns
+        ]
     return feature_list_idxs
 
-def aggregate_features(df: pd.DataFrame, feature_lists: dict, aggregation_func, na_values_strategy: str = "skip"):
+
+def aggregate_features(
+    df: pd.DataFrame,
+    feature_lists: dict,
+    aggregation_func,
+    na_values_strategy: str = "skip",
+):
     feature_list_idxs = get_feature_list_indexes(df, feature_lists)
     aggregated_features = {}
     for k in feature_list_idxs.keys():
         if feature_list_idxs[k]:
-            df_subset = df.iloc[:, feature_list_idxs[k][0]:feature_list_idxs[k][-1]+1]
+            df_subset = df.iloc[
+                :, feature_list_idxs[k][0] : feature_list_idxs[k][-1] + 1
+            ]
             if na_values_strategy == "zeros":
                 df_subset = df_subset.fillna(0)
             elif na_values_strategy == "mean":
                 df_subset = df_subset.fillna(df_subset.mean())
             aggregated_features[k] = df_subset.agg(aggregation_func, axis=1)
     return aggregated_features
+
 
 def features_to_drop_after_aggregation(df: pd.DataFrame, feature_lists: dict):
     feature_list_idxs = get_feature_list_indexes(df, feature_lists)
@@ -27,32 +39,36 @@ def features_to_drop_after_aggregation(df: pd.DataFrame, feature_lists: dict):
         features_to_drop += feature_list_idxs[k]
     return features_to_drop
 
+
 def features_with_too_many_nans(df: pd.DataFrame, nan_threshold: float):
     nan_percs = []
     for col in df.columns:
         nan_percs.append(df[col].isna().sum() / df.shape[0])
     return np.where(np.array(nan_percs) > nan_threshold)
 
+
 def custom_binary_agg(series):
     binary = [1 if x == 1 else 0 if x == 2 else x for x in series]
-    return sum(val*(2**idx) for idx, val in enumerate(reversed(binary)))
+    return sum(val * (2**idx) for idx, val in enumerate(reversed(binary)))
+
 
 def custom_sum(series):
     return series.sum(min_count=1)
 
+
 def custom_mean(series):
     return series.mean(skipna=True)
 
+
 ################### MIXED FEATURES ###################
+
 
 def get_good_bad_agg(row, group, aggregation_map, max_degree_of_agreement):
     no_good = (
-        len(aggregation_map["a"][group])
-        - row[aggregation_map["a"][group]].isna().sum()
+        len(aggregation_map["a"][group]) - row[aggregation_map["a"][group]].isna().sum()
     )
     no_bad = (
-        len(aggregation_map["b"][group])
-        - row[aggregation_map["b"][group]].isna().sum()
+        len(aggregation_map["b"][group]) - row[aggregation_map["b"][group]].isna().sum()
     )
 
     if no_good == 0 and no_bad == 0:
@@ -65,7 +81,8 @@ def get_good_bad_agg(row, group, aggregation_map, max_degree_of_agreement):
                 - row[aggregation_map["b"][group]].sum()
             )
         ) / (no_good + no_bad)
-    
+
+
 def aggregate_mixed_features(df, agg_map, lambda_agg):
     aggregated_features = {}
     for new_column, lambda_rule in lambda_agg.items():
@@ -75,6 +92,7 @@ def aggregate_mixed_features(df, agg_map, lambda_agg):
                 columns += cols_group
         aggregated_features[new_column] = df.loc[:, columns].agg(lambda_rule, axis=1)
     return aggregated_features
+
 
 def mixed_features_to_drop(df, mixed_features):
     features_to_drop = []
