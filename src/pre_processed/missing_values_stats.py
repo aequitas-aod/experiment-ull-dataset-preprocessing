0068b0985e4d78f26115d84ae365a6563b0145d4
Joseph Giovanelli <j.giovanelli@unibo.it>
2024-05-08 15:50:39 +0200

    Update files and stats

--- src/pre_processed/missing_values_stats.py
+++ src/pre_processed/missing_values_stats.py
@@ -8,14 +8,18 @@ from src.pre_processing.macros import (
     ORIGINAL_DATASET_NAME,
 )
 
+
 def print_missing_values_ranges(range_count: dict):
     for k, v in range_count.items():
-        print(f"Number of columns with a proportion of missing values in range {k}: {v}")
+        print(
+            f"Number of columns with a proportion of missing values in range {k}: {v}"
+        )
+
 
 def postprocessing_stats(df_orig: pd.DataFrame, df_merged: pd.DataFrame):
 
     # init ranges
-    ranges = [(0.0, 0.2), (0.2, 0.4), (0.4, 0.6), (0.6, 1.0)]
+    ranges = [(0.0, 0.2), (0.2, 0.4), (0.4, 0.6), (0.6, 0.8), (0.8, 1.0)]
     range_count = {str(range): 0 for range in ranges}
 
     # update_ranges
@@ -38,11 +42,22 @@ def postprocessing_stats(df_orig: pd.DataFrame, df_merged: pd.DataFrame):
     size_orig = df_orig.shape[0] * df_orig.shape[1]
     size_merged = df_merged.shape[0] * df_merged.shape[1]
 
-    print(f"Amount of data reduction: {((size_orig - size_merged) / size_orig) * 100} %")
-    print(f"Proportion of missing values before preprocessing: {(nans_orig/size_orig) * 100} %")
-    print(f"Proportion of missing values after preprocessing: {(nans_merged/size_merged) * 100} %")
+    print(
+        f"Amount of data reduction: {((size_orig - size_merged) / size_orig) * 100} %"
+    )
+    print(
+        f"Proportion of missing values before preprocessing: {(nans_orig/size_orig) * 100} %"
+    )
+    print(
+        f"Proportion of missing values after preprocessing: {(nans_merged/size_merged) * 100} %"
+    )
+
 
 if __name__ == "__main__":
-    df_orig = pd.read_csv(os.path.join(DATA_PATH, ORIGINAL_DATASET_NAME), low_memory=False)
-    df_merged = pd.read_csv(os.path.join(DATA_PREPROC_PATH, "merged.csv"), low_memory=False)
+    df_orig = pd.read_csv(
+        os.path.join(DATA_PATH, ORIGINAL_DATASET_NAME), low_memory=False
+    )
+    df_merged = pd.read_csv(
+        os.path.join(DATA_PREPROC_PATH, "merged.csv"), low_memory=False
+    )
     postprocessing_stats(df_orig=df_orig, df_merged=df_merged)
